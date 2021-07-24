145b1298a50f27731d141d4ff903ccc5a5edd758
jeffdaniel30 <jeffdaniel30@gmail.com>
2021-07-24 12:35:28 -0400

    cors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Plant from './Components/Plants/Plant'
 import PlantDetails from './Components/Plants/PlantDetails';
 import { BrowserRouter as Router } from "react-router-dom";
 import Backend from './Components/Backend'
+import axios from 'axios'
 
 function App() {
+  // pass plants into state
+  axios.get(`http://localhost:4000/plants/index`)
+  .then(res => console.log(res))
+
   return (
     <div className="App">
       <h1>Plant App</h1>    
