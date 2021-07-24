bce96ca2e8f3ea7faee80317105772b055ed5296
jeffdaniel30 <jeffdaniel30@gmail.com>
2021-07-24 10:18:22 -0400

    getting json

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Plant from './Components/Plants/Plant'
 import PlantDetails from './Components/Plants/PlantDetails';
 import { BrowserRouter as Router } from "react-router-dom";
-
+import Backend from './Components/Backend'
 
 function App() {
   return (
     <div className="App">
       <h1>Plant App</h1>    
       <Plant  />
-      <PlantDetails />
+      <PlantDetails/>
+      <Backend />
       </div>
   );
 }
---
+++ src/Components/Backend.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Plant from '../../../plantDaddies-backend/Controllers/Plants'
+
+const Backend = () => {
+    return (
+        <div>
+            <h1>Plants</h1>
+        </div>
+    )
+}
+
+export default Backend
