a65fb5dc5cbec1eedc1362610e84c50a82743c45
venkatnan <nanditavenkat@gmail.com>
2021-08-02 17:56:37 -0500

    form css added

--- src/Components/Plants/EditPlant.js
+++ src/Components/Plants/EditPlant.js
@@ -48,84 +48,59 @@ const EditPlant = ({ plants, match, setPlantData }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          type="name"
-          onChange={handleChange}
-          value={editPlant.name}
-        />
-        <label htmlFor="genus">Genus:</label>
-        <input
-          id="genus"
-          type="genus"
-          onChange={handleChange}
-          value={editPlant.genus}
-        />
-        <label htmlFor="timer">Timer:</label>
-        <input
-          id="timer"
-          type="timer"
-          onChange={handleChange}
-          value={editPlant.timer}
-        />
-        <label htmlFor="zone">Zone:</label>
-        <input
-          id="zone"
-          type="zone"
-          onChange={handleChange}
-          value={editPlant.zone}
-        />
-        <label htmlFor="water">Water:</label>
-        <input
-          id="water"
-          type="water"
-          onChange={handleChange}
-          value={editPlant.water}
-        />
-        <label htmlFor="image">Image:</label>
-        <input
-          id="image"
-          type="text"
-          alt={editPlant.name}
-          onChange={handleChange}
-          value={editPlant.image}
-        />
-        <label htmlFor="lightRequired">Light Required:</label>
-        <input
-          id="lightRequired"
-          type="lightRequired"
-          onChange={handleChange}
-          value={editPlant.lightRequired}
-        />
-        <label htmlFor="notes">Notes:</label>
-        <input
-          id="notes"
-          type="notes"
-          onChange={handleChange}
-          value={editPlant.notes}
-        />
-        <label htmlFor="fertilizer">Fertilizer:</label>
-        <input
-          id="fertilizer"
-          type="fertilizer"
-          onChange={handleChange}
-          value={editPlant.fertilizer}
-        />
-        <label htmlFor="houseplant">House Plant:</label>
-        <input
-          id="houseplant"
-          type="houseplant"
-          onChange={handleChange}
-          value={editPlant.houseplant}
-        />
-        <button type="submit">Save</button>
-      </form>
-      <form onSubmit={handleDelete}>
-        <button type="submit">Delete</button>
-      </form>
-    </div>
+            <h1> Edit {editPlant.name}</h1>
+        
+            <div id='editForm'>
+                <form onSubmit={handleSubmit}>
+
+                    <div class="row">
+                        <div class="col">
+                            <input id="name" placeholder="Plant Name" class="form-control" type="text" onChange={handleChange} value={editPlant.name}/>
+                        </div>
+                        <div class="col">
+                            <input id="genus" placeholder="Plant Genus" class="form-control" type="text" onChange={handleChange} value={editPlant.genus}/>
+                        </div>
+                        <div class="col">
+                            <input id="image" placeholder="Image URL" class="form-control" type="text" alt={editPlant.name} onChange={handleChange} value={editPlant.image}/>
+                        </div>
+                    </div>
+
+                    <div class='row'>
+                        <div class="col">
+                            <input id="lightRequired" placeholder="Light Requirements" class="form-control" type="text" onChange={handleChange} value={editPlant.lightRequired}/>
+                        </div>
+                        <div class="col">
+                            <input id="fertilizer" placeholder="Fertilizer" class="form-control" type="text" onChange={handleChange} value={editPlant.fertilizer}/>
+                        </div>
+                    <div class="col">
+                            <input id="water" placeholder="Water" class="form-control" type="text" onChange={handleChange} value={editPlant.water}/>
+                        </div>
+                    </div>
+
+                    <div class='group'>
+                        <input id="houseplant" placeholder="Is this a Houseplant?" class="form-control" type="text" onChange={handleChange} value={editPlant.houseplant}/>
+                    </div>
+
+                    <div class='row'>
+                        <div class="col">
+                            <input id="timer" placeholder="Timer" class="form-control" type="text" onChange={handleChange} value={editPlant.timer}/>
+                        </div>
+
+                        <div class="col">
+                            <input id="zone" placeholder="Zone" class="form-control"type="text" onChange={handleChange} value={editPlant.zone}/>
+                        </div>
+
+                        <div class="col">
+                            <input id="notes" placeholder="Extra Notes" class="form-control" type="text" onChange={handleChange} value={editPlant.notes}/>
+                        </div>
+                        
+                    </div>
+                    
+                    <button type="submit" class="btn btn-success">Submit Plant</button>
+                </form>
+                
+            </div>
+        </div>
   );
 };
 
