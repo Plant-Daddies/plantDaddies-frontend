35ba89294e12b7ef1604ecc4cf330c78d63b6a89
venkatnan <nanditavenkat@gmail.com>
2021-08-02 18:05:47 -0500

    buttons fixed

--- src/Components/Plants/Plant.js
+++ src/Components/Plants/Plant.js
@@ -6,10 +6,12 @@ import {Link} from 'react-router-dom'
 const Plant = (props) => {
   return (
     <div id="plantList">
+      
           {props.plants.map((plant) => (
             <div class="card" id='plantCard' key={plant.name}>
                 <Link to={`/plants/show/${plant.name}`}><img class="card-img-top" alt="Card cap"src={plant.image}></img></Link >
-                <Link to={{pathname:`/plants/edit/${plant._id}`, state:plant}}>Update</Link >
+                <Link to={{pathname:`/plants/edit/${plant._id}`, state:plant}} class='btn btn-success'>Update {plant.name}</Link >
+               
               <div lass="card-body">
                 <h5 class='card-title'> {plant.name} </h5>
                 <p class="card-text">Genus: {plant.genus} </p>
