bb0a3a3112a39cdee5d68c6354cf746700ffc4b7
venkatnan <nanditavenkat@gmail.com>
2021-08-02 23:14:44 -0500

    fonts

--- src/Components/Plants/SpecificPlant.js
+++ src/Components/Plants/SpecificPlant.js
@@ -1,6 +1,4 @@
-
 import React from 'react'
-
 import './specific.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,18 +7,19 @@ const SpecificPlant = ({plants, match, setNewPlant, plantData}) => {
     
     return (
         <div class="specified">
-
-            <h1>{plantDetail.name}</h1>
-            <h5> you have {(plantDetail.timer/86400)} days until {plantDetail.name} needs water</h5>
+            
+            <h1 id='plantHead'>The {plantDetail.name}</h1>
          <form>
-            <div class="card-deck">
+            <div >
                 <div class="card" id='card'>
                     <img class="card-img-top" src={plantDetail.image} alt={plantDetail.name} />
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Genus: {plantDetail.genus}</li>
                         <li class="list-group-item">Native Zone: {plantDetail.zone}</li>
-                        <li class="list-group-item">Is your {plantDetail.name} a House Plant? {plantDetail.houseplant.toString()}</li>
                     </ul>
+                    <div class="card-footer">
+                        <small class="text-muted">Is your {plantDetail.name} a House Plant? {plantDetail.houseplant.toString()}</small>
+                    </div>
                 </div>
 
                 <div class="card mb-3" id='card' >
@@ -30,10 +29,13 @@ const SpecificPlant = ({plants, match, setNewPlant, plantData}) => {
                         <li class="list-group-item">How much water does {plantDetail.name} need?:{plantDetail.water}</li>
                         <li class="list-group-item">How much light does {plantDetail.name} need?:{plantDetail.lightRequired}</li>
                         <li class="list-group-item">How much fertilizer does {plantDetail.name} need?:{plantDetail.fertilizer}</li>
+                        <li class="list-group-item">Extra Notes:{plantDetail.notes}</li>
                     </ul>
                     </div>
+                    <div class="card-footer">
+                        <small class="text-muted">you have {(plantDetail.timer/86400)} days until {plantDetail.name} needs water</small>
+                    </div>
                 </div>
-                <p>Extra Notes:{plantDetail.notes}</p>
             </div>
         </form>
         </div>
